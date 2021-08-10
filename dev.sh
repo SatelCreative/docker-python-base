13c1d437321f0cf298c3d1db111dd5f019c0a49f
Nathan-Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-08-10 10:16:57 -0700

    Changed to case instead of if

--- dev.sh
+++ dev.sh
@@ -3,12 +3,20 @@ docker build . -t satel/python-base:latest
 cd tests/
 docker build . -t docker-python-base
 
-if [[ $1 == "run" ]]; then
-    docker run -p 8000:8000 docker-python-base
-elif [[ $1 == "validate" ]]; then
-    docker run docker-python-base validatecodeonce
-elif [[ $1 == "cli" ]]; then
-    docker run -it docker-python-base /bin/bash
-else
-    echo "Provide parameters 'run', 'validate', or 'cli' when calling e.g. ./dev.sh run"
-fi
+case $1 in
+    run)
+        docker run -p 8000:8000 docker-python-base
+    ;;
+
+    validate)
+        docker run docker-python-base validatecodeonce
+    ;;
+
+    cli)
+        docker run -it docker-python-base /bin/bash
+    ;;
+    
+    *)
+        echo "Provide parameters 'run', 'validate', or 'cli' when calling e.g. ./dev.sh run"
+    ;;
+esac
