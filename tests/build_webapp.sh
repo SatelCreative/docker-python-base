2af2e554d686c59d0be39b8066df0c28f27c3a6d
Rahul <rahulpatidar0191@gmail.com>
2022-06-13 10:52:23 -0700

    handle exit code better

--- tests/build_webapp.sh
+++ tests/build_webapp.sh
@@ -6,13 +6,14 @@ docker build --target webapp_prd -t webapp_prd .
 
 docker run webapp_dev validatecodeonce; STATUS1=$?
 
-exit $STATUS1
-
 docker run -p 8000:8000 webapp_prd startapp
 
 sleep 5
 
 curl -L http://localhost:8000/; STATUS2=$?
 
-exit $STATUS2
+TOTAL=$((STATUS1 + STATUS2))
+exit $TOTAL
+
+
 
