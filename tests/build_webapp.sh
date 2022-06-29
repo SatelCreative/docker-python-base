0bfb19edd5c80e30a7fd5a56dc2b2c11492fbed2
Ant <anthony.hillairet@gmail.com>
2022-06-29 11:02:32 -0700

    Adapt to using build stage, rm explicit startapp

--- tests/build_webapp.sh
+++ tests/build_webapp.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-docker build --target webapp_dev -t webapp_dev .
+docker build --build-arg "--dev" -t webapp_dev .
 
-docker build --target webapp_prd -t webapp_prd .
+docker build -t webapp_prd .
 
 docker run webapp_dev validatecodeonce; STATUS1=$?
 
-docker run -p 8000:8000 -d webapp_prd startapp
+docker run -p 8000:8000 -d webapp_prd
 
 sleep 5
 
@@ -14,6 +14,3 @@ curl -L http://localhost:8000/; STATUS2=$?
 
 TOTAL=$((STATUS1 + STATUS2))
 exit $TOTAL
-
-
-
